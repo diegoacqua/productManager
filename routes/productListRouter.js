f8760bfaa123f427277f49ffbd814667f085a48f
Diego Acquaviva <diegojacqua@gmail.com>
2023-04-10 23:18:57 -0400

    se arreglaron errores en productListRouter y en app.js

--- routes/productListRouter.js
+++ routes/productListRouter.js
@@ -1,17 +1,6 @@
 import { Router } from "express";
 import { ProductManager } from "../public/productManager.js";
 
-/*export const getHome = async (req, res) => {
-  try {
-    const product = new ProductManager();
-    const productList = await product.getProducts();
-    res.render("products", { title: "Lista de productos", productList });
-  } catch (e) {
-    res.status(404).send(e);
-  }
-};
-*/
-
 const productListRouter = Router();
 
 const manager = new ProductManager();
@@ -19,6 +8,11 @@ const manager = new ProductManager();
 productListRouter.get("/", async (req, res) => {
   const productList = await manager.getProducts();
   res.render("products", { title: "Lista de productos", productList });
+});
+
+productListRouter.get("/liveProducts", async (req, res) => {
+  const productList = await manager.getProducts();
+  res.render("liveProducts", { title: "Lista de productos", productList });
 });
 
 export default productListRouter;
